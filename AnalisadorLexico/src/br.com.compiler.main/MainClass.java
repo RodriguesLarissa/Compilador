9c699f134f3266e1135d3e742a55801dbb865a2d
Lucas Cassin <lucas.yudi@aluno.ufabc.edu.br>
2022-06-29 22:38:56 -0300

    criado a classe main

--- AnalisadorLexico/src/br.com.compiler.main/MainClass.java
+++ AnalisadorLexico/src/br.com.compiler.main/MainClass.java
@@ -1,7 +1,15 @@
 package br.com.compiler.main;
 
+import AnalisadorLexico.src.br.com.compiler.lexico.ScannerComp;
+
 public class MainClass {
     public static void main(String[] args){
+        try {
+            ScannerComp sc = new ScannerComp("Input.txt");
 
+            System.out.println("Compilado");
+        } catch(Exception ex) {
+            System.out.println("Erro!");
+        }
     }
 }
