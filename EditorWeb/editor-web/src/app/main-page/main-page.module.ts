58fb1da7186b79a5707d1016685ccb1e3406060d
LahRodriigues <rodrigues_larissa@outlook.com>
2022-08-17 01:02:22 -0300

    Insercao do add file

--- EditorWeb/editor-web/src/app/main-page/main-page.module.ts
+++ EditorWeb/editor-web/src/app/main-page/main-page.module.ts
@@ -1,3 +1,4 @@
+import { ToolsBarComponent } from '../components/barra-ferramentas/tools-bar/tools-bar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,10 +6,11 @@ import { MainPageComponent } from '../main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ToolsBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [MainPageComponent]
