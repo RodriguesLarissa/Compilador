934a319ddff4ed6636a03e13b65c9f8fa643a38b
LahRodriigues <rodrigues_larissa@outlook.com>
2022-08-16 22:57:00 -0300

    Alteracao de nomes

--- EditorWeb/editor-web/src/app/main-page.module.ts
+++ EditorWeb/editor-web/src/app/main-page.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainPageComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainPageComponent]
 })
-export class AppModule { }
+export class MainPageModule { }
