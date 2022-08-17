df767801cf04523357ed5145a5b161d97e54fc30
LahRodriigues <rodrigues_larissa@outlook.com>
2022-08-16 22:59:52 -0300

    Insercao de codigo em pasta

--- EditorWeb/editor-web/src/app/main-page/main-page.module.ts
+++ EditorWeb/editor-web/src/app/main-page/main-page.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MainPageComponent } from './main-page.component';
+import { MainPageComponent } from '../main-page/main-page.component';
 
 @NgModule({
   declarations: [
